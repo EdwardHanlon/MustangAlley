b3eb9ab7963a75cc3b4d8cb77309a9786d1db7a1
EdwardHanlon <EJHanlon03@gmail.com>
2018-05-10 22:58:56 -0400

    Updated valid Model Years to go to 2019

--- MustangAlley/ViewModels/Registration/RegistrationViewModel.cs
+++ MustangAlley/ViewModels/Registration/RegistrationViewModel.cs
@@ -74,7 +74,7 @@ namespace MustangAlley.ViewModels.Registration
 
         #region Vehicle Properties
 
-        [RegularExpression(@"19(6[4-9]|[789]\d)|20(0\d|1[012345678])", ErrorMessage = "Please enter a valid vehicle year between 1964 and 2018")]
+        //[RegularExpression(@"19(6[4-9]|[789]\d)|20(0\d|1[0123456789])", ErrorMessage = "Please enter a valid vehicle year between 1964 and 2019")]
         [Display(Name = "Vehicle Year")]
         [DefaultValue(0)]
         public int Year { get; set; }
