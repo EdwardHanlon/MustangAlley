504280070defdc2223631e16460b1cec5e8325a8
EdwardHanlon <EJHanlon03@gmail.com>
2018-05-12 13:31:11 -0400

    Updated site.js to change field requirements when showing/hiding fields

--- MustangAlley/wwwroot/js/site.js
+++ MustangAlley/wwwroot/js/site.js
@@ -8,19 +8,20 @@ function showPreviousPosition() {
 
 function hideVehicleRegistration() {
     $('#vehicle').hide();
+
+    $('#BodyStyle').removeAttr('required');
+    $('#Color').removeAttr('required');
+    $('#LicensePlateNumber').removeAttr('required');
 }
 
 function showVehicleRegistration() {
     $('#vehicle').show();
+
+    $('#BodyStyle').prop('required', true);
+    $('#Color').prop('required', true);
+    $('#LicensePlateNumber').prop('required', true);
 }
 
-function hideVolunteerRegistration() {
-    $('#volunteer').hide();
-}
-
-function showVolunteerRegistration() {
-    $('#volunteer').show();
-}
 
 function recaptchaCallback() {
     $('#registerBtn').removeAttr('disabled');
